f15ebf040000142a74e26d25ea4761bdf5d46134
Espotyxs <114021463+Espotyxs@users.noreply.github.com>
2024-07-16 12:43:05 +0200

    Dev #2
    Made!

--- main.py
+++ main.py
@@ -1,1 +1,22 @@
-
+import random
+import string
+ras = random.randint(20,25)
+pos = 0
+rand = random.randint(1,3)
+pss = str()
+print("Password Generator, created by Xavier Sanchez (Espotyxs) 2024")
+while ras >= pos:
+    if rand == 1:
+        pss = (random.choice(string.ascii_uppercase) + pss )
+        pos = pos + 1
+        rand = random.randint(1,3)
+    elif rand == 2:
+        pss = (random.choice(string.ascii_lowercase) + pss )
+        pos = pos + 1
+        rand = random.randint(1,3)
+    else:
+        pss = (random.choice(string.ascii_letters) + pss)
+        pos = pos + 1
+print("# Your password is")
+print("#", pss)
+print("# Here you go! {xD")
